d7d964627b157ebaa3be4b722ad4e00b53a190b2
Даниил <deadxraver@yandex.ru>
2025-11-13 08:08:32 +0300

    понятнее отображение данных

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 s = ""
-global header
+header = []
 with open("california_housing_train.csv") as f:
 	s = f.readlines()
-	header = s.pop(0).split(';')
+	header = s.pop(0).replace('"', '').replace('\n', '').split(';')
 	for i in range(len(s)):
 		s[i] = list(map(float, s[i].split(';')))
 
+table = {}
+for i in range(len(header)):
+	table[header[i]] = []
+	for j in range(len(s)):
+		table[header[i]].append(s[j][i])
+
 number = len(s)
-avg = sum(row[-1] for row in s) / number
+avg = sum(table['median_house_value']) / number
 
 standart_otkl = ...
-minimum = min(row[-1] for row in s)
-maximum = max(row[-1] for row in s)
+minimum = min(table['median_house_value'])
+maximum = max(table['median_house_value'])
 
 print(number, avg, minimum, maximum)
